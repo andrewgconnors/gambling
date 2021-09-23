95c41dbc3e9949790218a334eedcf3d437b0a1b5
andrewgconnors <andrewgconnors@gmail.com>
2021-09-23 00:13:04 -0400

    Add teasers calculation page

---
+++ teasers.js
@@ -0,0 +1,56 @@
+function acceptEnter(event) {
+    if(event.key !== "Enter") return;
+    console.log("acceptEnter");
+    calculate();
+    event.preventDefault();
+}
+
+// document.querySelector("#teams").addEventListener("keyup", acceptEnter);
+
+function americanToDecimal(odds) {
+    if (odds[0] === '+')
+        return 1 + (parseFloat(odds.substring(1)) / 100);
+    else if (odds[0] === '-') {
+        var risk = parseFloat(odds.substring(1));
+        return 1 + (100 / risk);
+    }
+}
+
+function calculate() {
+    var team_probs = {};
+    var combos = [];
+    var odds = {};
+    var units = {};
+
+    var ev = {};
+    var variance = {};
+
+    var teamLines = document.getElementById('teams').value.split("\n");
+    for (let line of teamLines) {
+        let split = line.trim().split(/\s+/);
+        team_probs[split[0].trim()] = parseFloat(split[1].trim());
+    }
+    // Computer lines, not betting lines
+    var comboLns = document.getElementById('combos').value.split("\n");
+    for (let line of comboLns) {
+        let split = line.trim().split(/\s+/);
+        let combo = split.slice(0, -2).map(s => s.trim());
+        combos.push(combo);
+        odds[combo] = split[split.length - 2].trim();
+        units[combo] = parseFloat(split[split.length - 1].trim());
+    }
+    console.log(team_probs);
+    console.log(combos);
+    console.log(odds);
+    console.log(units);
+
+    document.getElementById('stats').innerHTML = "";
+
+    for (let combo of combos) {
+        let p_win = combo.map(team => team_probs[team]).reduce((prev, cur) => prev*cur, 1);
+        let reward = (americanToDecimal(odds[combo]) - 1) * units[combo];
+        ev[combo] = p_win * reward - ((1 - p_win) * units[combo]);
+        variance[combo] = p_win * Math.pow((reward - ev[combo]), 2) + (1 - p_win) * Math.pow((-1*units[combo] - ev[combo]), 2);
+        document.getElementById('stats').insertAdjacentHTML('beforeend', `<tr><td>${combo}</td><td>${ev[combo]}</td><td>${variance[combo]}</td></tr>`)
+    }
+}
