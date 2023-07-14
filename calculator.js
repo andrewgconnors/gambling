52c5fca13dd7ce946f2265367c77bb126651f2c9
andrewgconnors <andrewgconnors@gmail.com>
2023-07-13 22:19:27 -0400

    Change var to let in calculator.js

--- calculator.js
+++ calculator.js
@@ -5,19 +5,19 @@ document.querySelector("#lines").addEventListener("keyup", event => {
 });
 
 function calculate() {
-    var oddsList = document.getElementById('lines').value;
+    let oddsList = document.getElementById('lines').value;
     oddsList = oddsList.replaceAll('-', ',-');
     oddsList = oddsList.replaceAll('+', ',+');
     oddsList = oddsList.split(",");
-    var impliedProbs = [];
-    for (var odds of oddsList) {
+    let impliedProbs = [];
+    for (let odds of oddsList) {
         odds = odds.trim();
         if (odds.length === 0)
             continue;
         else if (odds[0] === '+')
             impliedProbs.push(100.0/(100.0 + parseFloat(odds.substring(1))));
         else if (odds[0] === '-') {
-            var risk = parseFloat(odds.substring(1));
+            let risk = parseFloat(odds.substring(1));
             impliedProbs.push(risk/(risk + 100.0));
         }
         else {
@@ -26,12 +26,12 @@ function calculate() {
         }
     }
     document.getElementById('impl_probs').innerHTML = impliedProbs.map(odds => odds.toFixed(4)).toString();
-    var impliedSum = impliedProbs.reduce((prev, current) => prev + current, 0);
+    let impliedSum = impliedProbs.reduce((prev, current) => prev + current, 0);
     document.getElementById('overround').innerHTML = impliedSum.toFixed(4).toString();
-    var vig = 1 - 1/impliedSum;
+    let vig = 1 - 1/impliedSum;
     document.getElementById('vig').innerHTML = (100*vig).toFixed(2).toString() + "%";
-    var adjustedProbs = impliedProbs.map(p => p/impliedSum);
+    let adjustedProbs = impliedProbs.map(p => p/impliedSum);
     document.getElementById('adj_probs').innerHTML = adjustedProbs.map(odds => odds.toFixed(4)).toString();
-    var adjustedOdds = adjustedProbs.map(p => (p > 0.5 ? "-" + (100.0*p/(1-p)).toFixed(2).toString() : "+" + ((100.0 - 100.0*p)/p).toFixed(2).toString()));
+    let adjustedOdds = adjustedProbs.map(p => (p > 0.5 ? "-" + (100.0*p/(1-p)).toFixed(2).toString() : "+" + ((100.0 - 100.0*p)/p).toFixed(2).toString()));
     document.getElementById('vigfree_odds').innerHTML = adjustedOdds.toString();
 }
